b8cbc9e9553b034ac62e04da10dd75730daf8bc3
Rob <ramulcrone@gmail.com>
2021-02-07 19:00:11 +0800

    deploy code added

--- server.js
+++ server.js
@@ -20,13 +20,19 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-
-
 // Routes
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ptaSystemDesign");
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/ptaSystemDesign',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
