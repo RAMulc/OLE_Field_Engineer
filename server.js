a43f65ab30c43a0b56d52ad4c2e7e3d84825956f
Rob <ramulcrone@gmail.com>
2021-02-08 18:57:20 +0800

    local version running

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+app.use(express.static("client/build"));
+// }
 
 // Routes
 app.use(routes);
